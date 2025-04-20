cb566183a3430193c1f89740bb466da5c555c5c3
Vu Quoc Bao <quocbao24748@gmail.com>
2025-04-20 19:10:01 +0700

    tao nhanh model theo mvc

--- login/login.php
+++
@@ -1,7 +0,0 @@
-<?php<!-- HTML form đăng nhập -->
-<form method="POST" action="login.php">
-    <input type="text" name="username" placeholder="Username" required>
-    <input type="password" name="password" placeholder="Password" required>
-    <button type="submit">Login</button>
-</form>
-?>
