0ab87ef656970a057d37e8606d3b263b6c000083
Vu Quoc Bao <quocbao24748@gmail.com>
2025-04-20 19:13:41 +0700

    mo hinh mvc mau

---
+++ mvc/config.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    'base' => '/mvc/',
+    'assets' => '/mvc/assets/',
+    'baseURL' => 'http://localhost/mvc/',
+    'db' => array(
+        'host'     => 'localhost',
+        'name'     => 'ProductDB',
+        'username' => 'root',
+        'password' => ''
+    )
+);
