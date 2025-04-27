b8fc0d2fe5e33a6a9b039cecde3b27089daefb94
Vu Quoc Bao <quocbao24748@gmail.com>
2025-04-27 23:10:54 +0700

    up game(shop)page

--- 404/404.php
+++ 404/404.php
@@ -3,6 +3,8 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <link rel="stylesheet" href="../header/header.css">
+  <link rel="stylesheet" href="../footer/footer.css">
   <title>BayGorn1 - 404</title>
 </head>
 <body>
