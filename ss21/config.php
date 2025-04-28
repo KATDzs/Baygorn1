8921b5804a587e660694eb06c7d5d8f802054a22
Vu Quoc Bao <quocbao24748@gmail.com>
2025-04-28 08:18:58 +0700

    chỉnh sửa controller Auth

---
+++ ss21/config.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    'base' => '/ss21/',
+    'assets' => '/ss21/assets/',
+    'baseURL' => 'http://localhost/ss21/', 
+    'db' => array(
+        'host'     => 'localhost',
+        'name'     => 'Productdb',
+        'username' => 'root',
+        'password' => ''
+    )
+);
